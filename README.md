1851555b620eb126f8212a64d212e43be3677b5c
husein zolkepli <husein.zol05@gmail.com>
2021-09-08 19:50:06 +0800

    added install.sh

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Elasticsearch
 Store general purpose for texts. If you part of the organization, you can access to EK!
+
+## how-to
+
+1. Install in private machine,
+
+```bash
+sudo bash install.sh
+```
---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install openjdk-8-jdk
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt-get update && sudo apt-get install elasticsearch kibana -y
+
+sudo update-rc.d elasticsearch defaults 95 10
+sudo update-rc.d kibana defaults 95 10
+sudo -i service elasticsearch start
+sudo -i service kibana start
