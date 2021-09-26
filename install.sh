7d9dfb25e25358654b82efe7b4fae0eed44e9470
husein zolkepli <husein.zol05@gmail.com>
2021-09-26 22:54:18 +0800

    use jdk 11

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install openjdk-8-jdk -y
+sudo apt install openjdk-11-jdk -y
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
